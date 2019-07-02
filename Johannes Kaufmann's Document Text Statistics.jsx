3f05000e348a295540e1d46da4891fbc18c3af5a
JohnLinux2003 <48015801+JohnLinux2003@users.noreply.github.com>
2019-07-02 22:32:34 +0200

    Version 1.0
    The original version of Johannes Kaufmann's Document Text Statistics tool in JavaScript including word and character counter for Adobe InDesign documents and projects.
    
    --with instalation file--

---
+++ Johannes Kaufmann's Document Text Statistics.jsx
@@ -0,0 +1,34 @@
+﻿if ( app.documents.length > 0 ) {
+    var
+        mD = app.activeDocument,
+        dS = mD.stories,
+        eTB = dS.length;
+    if ( dS.length > 0 ) {
+        var
+            dS = dS.everyItem(),
+            dWord = dS.words.length,
+            dFword = dS.footnotes.everyItem().words.length,
+            dTword = ( dS.tables.length > 0 ? dS.tables.everyItem().cells.everyItem().words.length : 0 ),
+            eC = dS.characters.length,
+            lF = dS.length,
+            aC = eC / eTB,
+            aW = dWord / eTB,
+            aWr = Math.round(aW),
+            dReport = [];
+           
+            dReport.push ( "Document text statistics:", "===================", "" );
+            dReport.push ( "Words", "------------------------------", "= " + dWord, "" );
+            dReport.push ( "Characters", "------------------------------", "= " + eC, "" );
+            dReport.push ("Textelements (textframes)", "------------------------------", "= " + eTB, "" );
+            dReport.push ( "Letters in each textframe", "------------------------------", "= " + lF, "" );
+            dReport.push ( "Average characters per textframe", "------------------------------", "= " + Math.round(aC) , "" );
+            dReport.push ( "Average words per textframe", "------------------------------", "= " + aWr , "" );
+            dReport.push ( "", "", "" ,"Johannes Kaufmann©2019" );
+            
+            alert ( dReport.join ( "\r" ), "Johannes Kaufmann's Document Text Statistics" );
+    } else {
+        alert ( "No textframes found in the document!", "Document Text Statistic" );
+    }
+} else {
+    alert ( "No opened document!", "Document Text Statistic" );
+}
